ca84fa276c1c23b2b0ef7d176dcd2d72263d525f
Tomasz Jurtsch <tjurtsch@antmicro.com>
2020-11-09 14:09:22 +0100

    wip

--- zachjs-sv2v/build.sh
+++ zachjs-sv2v/build.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+#cp -r $PREFIX/lib/* $PREFIX/x86_64-conda-linux-gnu/sysroot/usr/lib/
+#cp -r $PREFIX/lib64/* $PREFIX/x86_64-conda-linux-gnu/sysroot/usr/lib64/
+
+#export GMP_LIB_DIRS="$PREFIX/lib"
+#export GMP_INCLUDE_DIRS="$PREFIX/include"
 make
 install -D bin/sv2v $PREFIX/bin/zachjs-sv2v
 
