acc2c4f16ef7d60d1dd284e62e2c0c69a73ad151
Tomasz Jurtsch <tjurtsch@antmicro.com>
2020-12-07 14:49:20 +0100

    Save script output to file

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -26,7 +26,7 @@ if [[ $OS_NAME != 'windows' ]]; then
     fi
 else
     # Work-around: prevent console output being mangled
-    conda build $CONDA_BUILD_ARGS
+    conda build $CONDA_BUILD_ARGS | tee /tmp/output.log
 fi
 end_section "conda.build"
 
