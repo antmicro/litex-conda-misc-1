8e7a8f9873891efda277d84cea9681acfa084e72
Tomasz Jurtsch <tjurtsch@antmicro.com>
2020-12-08 12:40:53 +0100

    debug: additional prints when downloading macOS SDK

--- .github/scripts/download_sdk.sh
+++ .github/scripts/download_sdk.sh
@@ -1,5 +1,8 @@
 if [ $OS_NAME = 'osx' ]; then
     if [[ ! -d $HOME/sdk/MacOSX10.9.sdk ]]; then
+	echo "Cloning MACOS SDK to $HOME/sdk"
         git clone https://github.com/phracker/MacOSX-SDKs $HOME/sdk
+    else
+	echo "NOT Cloning MACOS SDK to $HOME/sdk. It already exists."
     fi
 fi
